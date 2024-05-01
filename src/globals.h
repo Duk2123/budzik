73075768f4d7e7a0020fd52fe4fc157180f6cf8c
Duk2123 <paweldyczek02@gmail.com>
2024-05-01 21:45:18 +0200

    reworked touch detection and handling

--- src/globals.h
+++ src/globals.h
@@ -7,5 +7,6 @@
 
 #include <vector>
 #include <map>
+#include <queue>
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <main.h>
 #include <screens/screens.h>
 
+typedef struct
+{
+  int x;
+  int y;
+} coordinates;
+
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite clockSprite = TFT_eSprite(&tft);
 
@@ -9,10 +15,15 @@ TFT_eSprite clockSprite = TFT_eSprite(&tft);
     0 - long press
     1 - swipe
     2 - press
- touchQueuedAction[1] - x coordinates or deviation in case of swipe
- touchQueuedAction[2] - y coordinates or deviation in case of swipe
+ touchQueuedAction[1] - x coordinates
+ touchQueuedAction[2] - y coordinates
+ touchQueuedAction[3]* - x deviation
+ touchQueuedAction[4]* - y deviation
+ touchQueuedAction[5]* - angle of swipe [-180,180] 0:up, -90:left, 90:right, -180/180:down
+ * only for swipe
 */
-int touchQueuedAction[6]; // TODO przerobić na powiadomienia z rtos albo coś innego
+int touchCurrentAction[6]; // TODO przerobić na powiadomienia z rtos albo coś innego
+
 int prevBrightness;
 int brightness;
 void setBrightness(int value)
@@ -26,31 +37,74 @@ TaskHandle_t updateDisplay_t;
 TaskHandle_t handlePopUp_t;
 
 SemaphoreHandle_t tftMutex = xSemaphoreCreateMutex();
+QueueHandle_t touchQueue = xQueueCreate(43, sizeof(coordinates));
+bool isTouchProcessing = false;
 
-void handleTouch(int *touchQueuedAction)
+TaskHandle_t handleTouch_t;
+// Task that reads data send from detectTouch task in queue and processes it setting
+void handleTouch(void *params)
 {
-  xSemaphoreTake(tftMutex, portMAX_DELAY);
+  coordinates coords;
+  int devX, devY, firstX, firstY, i;
+  for (;;)
   {
-    activeScreenElement->processTouch(touchQueuedAction);
+    // Wait for data
+    if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY) != 0)
+    {
+      isTouchProcessing = true; // Set flag to stop notifications
+      // Processing data from queue
+      xQueuePeek(touchQueue, &(coords), pdMS_TO_TICKS(150));
+      i = 1;
+      firstX = coords.x;
+      firstY = coords.y;
+      while (xQueueReceive(touchQueue, &(coords), pdMS_TO_TICKS(150)))
+      {
+        i++;
+        devX = firstX - coords.x;
+        devY = firstY - coords.y;
+        // Detecting swipe
+        if (i > 4 && (abs(devX) > 50 || abs(devY) > 30 || abs(devX) + abs(devY) > 58))
+        {
+          touchCurrentAction[0] = 1;
+          touchCurrentAction[1] = firstX;
+          touchCurrentAction[2] = firstY;
+          touchCurrentAction[3] = -devX;
+          touchCurrentAction[4] = devY;
+          touchCurrentAction[5] = atan2(-devX, devY) * 180 / 3.14159265;
+        }
+        // Detecting long press
+        else if (i > 31)
+        {
+          touchCurrentAction[0] = 0;
+          touchCurrentAction[1] = coords.x;
+          touchCurrentAction[2] = coords.y;
+        }
+        // Detecting press
+        else
+        {
+          touchCurrentAction[0] = 2;
+          touchCurrentAction[1] = coords.x;
+          touchCurrentAction[2] = coords.y;
+        }
+        vTaskDelay(4);
+      }
+    }
+    isTouchProcessing = false; // Set flag to turn on notifications
   }
-  xSemaphoreGive(tftMutex);
 }
 
 TaskHandle_t detectTouch_t;
-// Task for touch detection, upon detecting touch calls handleTouch function with touch type and coordinates or deviation as arguments.
+// Task for touch detection, upon detecting touch sends notification to handleTouch task to process data
 void detectTouch(void *params)
 {
 
   u_int16_t x, y, vx, vy, x2, y2, x3, y3, x4, y4;
-  std::pair<int, int> touchBufor[43];
-  std::fill(std::begin(touchBufor), std::end(touchBufor), std::pair<int, int>(0, 0));
-  int i = 0;      // touchBufor index
   int devX, devY; // axis deviation for swipe
   for (;;)
   {
     // Serial.println("x");
     //  Check if screen is being touched and no long press detected
-    if ((analogRead(8) < 1000) && i != 43)
+    if ((analogRead(8) < 1000))
     {
 
       // Touch threshold
@@ -78,65 +132,24 @@ void detectTouch(void *params)
             }
             else
             {
-              touchBufor[i] = {x, y};
-              i++;
+              // Send coordinates to queue
+              coordinates temp = {x, y};
+              if (uxQueueSpacesAvailable(touchQueue) == 0)
+              {
+                xQueueReceive(touchQueue, &(temp), (TickType_t)0);
+              }
+              xQueueSendToBack(touchQueue, &(temp), (TickType_t)0);
             }
           }
         }
       }
       xSemaphoreGive(tftMutex);
-
-      vTaskDelay(7);
-    }
-    // If screen is no longer touched or touched for max amount of time (touchBufor[size] * vTaskDelay(ms))
-    else if (i > 0)
-    {
-      // Detect long press
-      if (i >= 32)
+      // Notify the handleTouch task when it isn't running
+      if (isTouchProcessing == false)
       {
-        touchQueuedAction[0] = 0;
-        touchQueuedAction[1] = touchBufor[i - 1].first;
-        touchQueuedAction[2] = touchBufor[i - 1].second;
+        vTaskNotifyGiveFromISR(handleTouch_t, NULL);
       }
-      else
-      {
-        // Detect swipe
-        devX = 0;
-        devY = 0;
-        for (int j = 0; j < i; j++)
-        {
-          devX += touchBufor[i].second;
-          devY += touchBufor[i].first;
-        }
-        devX *= 1 / i;
-        devY *= 1 / i;
-        devX += touchBufor[0].first - touchBufor[i - 1].first;
-        devY += touchBufor[0].second - touchBufor[i - 1].second;
-        if (abs(devX) > 60 || abs(devY) > 30 || abs(devX) + abs(devY) > 67)
-        {
-          touchQueuedAction[0] = 1;
-          touchQueuedAction[1] = touchBufor[0].first;
-          touchQueuedAction[2] = touchBufor[0].second;
-          touchQueuedAction[3] = -devX;
-          touchQueuedAction[4] = devY;
-          touchQueuedAction[5] = atan2(-devX, devY) * 180 / 3.14159265;
-        }
-
-        // Detect press
-        else
-        {
-          touchQueuedAction[0] = 2;
-          touchQueuedAction[1] = touchBufor[i - 1].first;
-          touchQueuedAction[2] = touchBufor[i - 1].second;
-        }
-      }
-      // TODO Wywołać przerwanie i podać argumenty do taska
-      handleTouch(touchQueuedAction);
-
-      // Clear touchBufor and reset its index
-      std::fill(std::begin(touchBufor), std::end(touchBufor), std::pair<int, int>(0, 0));
-      i = 0;
-      vTaskDelay(500);
+      vTaskDelay(8);
     }
     // Screen was not touched
     else
@@ -155,14 +168,15 @@ void setup(void)
 
   tft.init();
   tft.setRotation(1);
-  setBrightness(100);
+  setBrightness(125);
 
   uint16_t calData[5] = {225, 3765, 200, 3765, 7};
   tft.setTouch(calData);
 
   tft.fillScreen(0);
 
-  xTaskCreate(detectTouch, "touch", 10024, NULL, 1, &detectTouch_t);
+  xTaskCreate(detectTouch, "detectTouch", 20048, NULL, 1, &detectTouch_t);
+  xTaskCreate(handleTouch, "handleTouch", 20048, NULL, 1, &handleTouch_t);
   menuScreen(NULL);
 
   Serial.println("Running...");
@@ -170,4 +184,21 @@ void setup(void)
 
 void loop()
 {
+  if (touchCurrentAction[0] != -1)
+  {
+    if (touchCurrentAction[0] == 1)
+    {
+      Serial.printf("x: %d y: %d devx: %d devy: %d deg: %d\n", touchCurrentAction[1], touchCurrentAction[2], touchCurrentAction[3], touchCurrentAction[4], touchCurrentAction[5]);
+    }
+    else
+    {
+
+      Serial.printf("type: %d x: %d y: %d\n", touchCurrentAction[0], touchCurrentAction[1], touchCurrentAction[2]);
+    }
+    touchCurrentAction[0] = -1;
+  }
+  else
+  {
+    delay(150);
+  }
 }
